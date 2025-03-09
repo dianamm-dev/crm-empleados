29b1b0d4c685867613762443bb7c903ae0acdf04
diana <dianamorcillom@gmail.com>
2025-03-09 21:04:51 +0100

    Creado el componente employee-detail: - Ruta desde ver empleado con su id

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { EmployeeDetailComponent } from '../components/employee-detail/employee-detail.component';
 import { EmployeePageComponent } from '../components/employee-page/employee-page.component';
 
 export const routes: Routes = [
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path: 'empleados',
         component: EmployeePageComponent
+    },
+    {
+        path: 'detalle-empleado/:id',
+        component: EmployeeDetailComponent
     }
 ];

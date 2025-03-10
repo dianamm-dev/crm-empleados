e04bcf3c562b654470d5d5e15702344329888b79
emcarnea <evacar02@gmail.com>
2025-03-11 00:44:57 +0100

    ajuste estilos responsive en bodycomponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,25 @@
 import { Routes } from '@angular/router';
 import { EmployeePageComponent } from '../components/employee-page/employee-page.component';
+import { BodyComponent } from './body/body.component';
 
 export const routes: Routes = [
     {
-        path: '',
+       path: '',
         redirectTo: 'empleados',
         pathMatch: 'full'
     },
     {
         path: 'empleados',
         component: EmployeePageComponent
-    }
+    },
+
+    /* {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: BodyComponent
+    } */
 ];

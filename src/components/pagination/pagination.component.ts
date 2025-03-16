01ad7c54473a2f67658ee2f5e5863f83c8871ec7
diana <dianamorcillom@gmail.com>
2025-03-16 21:38:31 +0100

    Creado componente pagination: - Selecciona número de empleados mostrados por página - Cambia la página en función de los empleados a mostrar

---
+++ src/components/pagination/pagination.component.ts
@@ -0,0 +1,60 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-pagination',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pagination.component.html',
+  styleUrl: './pagination.component.css'
+})
+export class PaginationComponent implements OnInit {
+  selectedNumber: number = 10; // 10 resultados por defecto
+  selectedPageNumber: number = 1; // primera página por defecto
+  pagesNumber: number[] = [];
+
+  @Input() resultsLength = 0;
+
+  @Output() resultNumberChanged = new EventEmitter<number>();
+  @Output() pageNumberChanged = new EventEmitter<number>();
+
+  ngOnInit(): void {
+    this.calculatePages();
+
+    // emito nuevo estado por defecto
+    this.resultNumberChanged.emit(this.selectedNumber);
+  }
+
+  setResultNumber(value: number) {
+    // actualizo el número seleccionado
+    this.selectedNumber = value;
+
+    // recalculo las páginas
+    this.calculatePages();
+
+    // selecciono por defecto la primera página
+    this.setPageNumber(1);
+
+    // emito un nuevo valor
+    this.resultNumberChanged.emit(value);
+  }
+
+  setPageNumber(value: number) {
+    if (value < 1 || value > this.pagesNumber.length) {
+      return; // evita cambiar si está fuera de los límites
+    }
+
+    this.selectedPageNumber = value;
+
+    // emito un nuevo valor
+    this.pageNumberChanged.emit(value);
+  }
+
+  calculatePages() {
+    // obtengo el número de paginas totales según el número de empleados
+    const numberOfPages = Math.ceil(this.resultsLength / this.selectedNumber);
+
+    // obtengo el array de las páginas
+    this.pagesNumber = Array(numberOfPages).fill(1).map((x, i) => i + 1);
+  }
+}

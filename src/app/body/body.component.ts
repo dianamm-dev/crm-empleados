9617779be68472d5af7f214b7b0d9027ebe96bcc
emcarnea <evacar02@gmail.com>
2025-03-18 20:19:04 +0100

    color ravadisa nombre login

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../auth/services/auth.service';
 
 @Component({
   selector: 'app-body',
@@ -9,4 +10,6 @@ import { RouterLink } from '@angular/router';
   styleUrls: ['./body.component.css']
 })
 export class BodyComponent {
+  public authService: AuthService = inject(AuthService);
+  
 }
